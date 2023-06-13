c3247ffc0e86800a8366002351e89ffa84201a70
Ivan Kor <helloivankor@gmail.com>
2023-06-13 12:21:02 +0300

    add graph

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
-              v-model="ticker"
-              v-on:keydown.enter="add"
+                v-model="ticker"
+                v-on:keydown.enter="add"
                 type="text"
                 name="wallet"
                 id="wallet"
@@ -73,11 +73,15 @@
           <div
             v-for="t in tickers"
             :key="t.name"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            @click="selectTicker(t)"
+            :class="{
+              'border-purple-800': select === t,
+            }"
+            class="bg-white overflow-hidden shadow rounded-lg border-4 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
+                {{ t.name.toUpperCase() }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
                 {{ t.price }}
@@ -85,7 +89,7 @@
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click="handleDelete(t)"
+              @click.stop="handleDelete(t)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -106,17 +110,23 @@
         </dl>
       </template>
       <hr class="w-full border-t border-gray-600 my-4" />
-      <section class="relative">
+      <section v-if="select" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{ select.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div class="bg-purple-800 border w-10 h-24"></div>
-          <div class="bg-purple-800 border w-10 h-32"></div>
-          <div class="bg-purple-800 border w-10 h-48"></div>
-          <div class="bg-purple-800 border w-10 h-16"></div>
+          <div
+            v-for="(bar, idx) in normalizeGraph()"
+            :key="idx"
+            :style="{ height: `${bar}%` }"
+            class="bg-purple-800 border w-10"
+          ></div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        <button
+          @click="select = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -145,17 +155,17 @@
 </template>
 
 <script>
+import API_KEY from "./config";
+
 export default {
   name: "App",
 
   data() {
     return {
-      ticker: "default",
-      tickers: [
-        { name: "Demo1", price: "-" },
-        { name: "Demo2", price: "2" },
-        { name: "Demo3", price: "-" },
-      ],
+      ticker: "",
+      tickers: [],
+      select: null,
+      graph: [],
     };
   },
 
@@ -167,12 +177,39 @@ export default {
       };
 
       this.tickers.push(currentTicker);
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=${API_KEY}`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === currentTicker.name).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if (this.select?.name === currentTicker.name) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
+
       this.ticker = "";
     },
 
+    selectTicker(ticker) {
+      this.select = ticker;
+      this.graph = [];
+    },
+
     handleDelete(tickerToRemove) {
-      this.tickers = this.tickers.filter(t => t !== tickerToRemove)
-    }
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      this.select = null;
+    },
+
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
   },
 };
 </script>
